4c39522328dab7aa57949f14400fee5075e24a07
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-27 15:35:18 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,31 @@
+def printxyzt(values):
+    print("x,y,z,time="+str(values))
+
+def printv(a):
+    for aa in a:
+        printxyzt(aa)
+
+def build(lena,incx,incy,incz,inct):
+    x=0
+    y=0
+    z=0
+    t=0
+    list1=[]
+    for n in range(lena):
+        l1=[x,y,z,t]
+        list1=list1+[l1]
+        x=x+incx
+        y=y+incy
+        z=z+incz
+        t=t+inct
+    return list1 
+def saves(names,list1):
+    f1=open(names,"w")
+    for n in list1:
+        x,y,z,t=n
+        f1.write(str(x)+", "+str(y)+", "+str(z)+", "+str(t)+"\n")
+    f1.close()
+print("\033c\033[43;30m\n")
+aaa=build(80,5,4,2,1)
+printv(aaa)
+saves("data.csv",aaa)
